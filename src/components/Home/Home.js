d1fca16e36bed45b1c9454269ed7d0205a92995f
Ravi Soni <soniravi829@gmail.com>
2019-05-18 23:21:46 +0530

    Add basic fetch and pagination

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,18 +4,54 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 /* TODO: comment out this logger when in production */
-import logQueryResult from '../../utils/dev/logQueryResult';
+// import logQueryResult from '../../utils/dev/logQueryResult';
 
-const POSTS_PER_PAGE = 3;
+const POSTS_PER_PAGE = 2;
 
 const Home = props => {
-  logQueryResult(props, 'posts', true); // TODO: comment out  in production
+  // logQueryResult(props, 'posts', true); // TODO: comment out  in production
+  let {
+    data: { loading, error, posts, postsConnection },
+    showMoreReviews,
+  } = props;
 
-  return (
-    <div>
-      <Button>A dumb button</Button>
-    </div>
-  );
+  if (error) {
+    return <h1>Error fetching posts!</h1>;
+  }
+
+  if (posts && postsConnection) {
+    const areMorePosts = posts.length < postsConnection.aggregate.count;
+    return (
+      <section>
+        {areMorePosts ? (
+          <Button disabled={loading} onClick={() => showMoreReviews()}>
+            {loading ? 'Loading...' : 'Load more reviews'}
+          </Button>
+        ) : (
+          <Button disabled>Load more reviews</Button>
+        )}
+
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <div>
+                <h3>Title: {post.title}</h3>
+                <p>Review: {post.body}</p>
+                <img
+                  alt={post.title}
+                  src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${
+                    post.image.handle
+                  }`}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return <h2>Loading posts...</h2>;
 };
 
 export const posts = gql`
@@ -23,11 +59,17 @@ export const posts = gql`
     posts(orderBy: title_ASC, first: $first, skip: $skip) {
       title
       body
+      id
       reviewer {
         username
       }
       image {
-        url
+        handle
+      }
+    }
+    postsConnection {
+      aggregate {
+        count
       }
     }
   }
@@ -43,4 +85,23 @@ export default graphql(posts, {
     variables: postQueryVars,
     notifyOnNetworkStatusChange: true,
   },
+  props: ({ data }) => ({
+    data,
+    showMoreReviews: () => {
+      return data.fetchMore({
+        variables: {
+          skip: data.posts.length,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          if (!fetchMoreResult) {
+            return previousResult;
+          }
+
+          return Object.assign({}, previousResult, {
+            posts: [...previousResult.posts, ...fetchMoreResult.posts],
+          });
+        },
+      });
+    },
+  }),
 })(Home);
