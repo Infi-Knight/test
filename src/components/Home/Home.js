b99ba3fefc0580fedc9d4a21bfe63bc7864703d9
Ravi Soni <soniravi829@gmail.com>
2019-05-18 07:36:30 +0530

    Create a simple logger for gql query results

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Button } from 'baseui/button';
-// import { Link } from 'react-router-dom';
-// import { graphql } from 'react-apollo';
-// import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const Home = () => {
+/* TODO: comment out this logger when in production */
+import logPostsFetch from '../../utils/dev/logQueryResult';
+
+const POSTS_PER_PAGE = 3;
+
+const Home = props => {
+  logPostsFetch(props, 'posts', true); // TODO: comment out  in production
+
   return (
     <div>
       <Button>A dumb button</Button>
@@ -12,4 +18,29 @@ const Home = () => {
   );
 };
 
-export default Home;
+export const posts = gql`
+  query posts($first: Int!, $skip: Int!) {
+    posts(orderBy: title_ASC, first: $first, skip: $skip) {
+      title
+      body
+      reviewer {
+        username
+      }
+      image {
+        url
+      }
+    }
+  }
+`;
+
+export const postQueryVars = {
+  skip: 0,
+  first: POSTS_PER_PAGE,
+};
+
+export default graphql(posts, {
+  options: {
+    variables: postQueryVars,
+    notifyOnNetworkStatusChange: true,
+  },
+})(Home);
