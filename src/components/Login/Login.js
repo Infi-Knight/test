d990d1bc3ac086996f1e3afc283dfbb1c50522c3
Ravi Soni <soniravi829@gmail.com>
2019-05-22 12:06:37 +0530

    Add Login boilerplate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
-const Login = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
-);
+import { AUTH_TOKEN } from '../../constants';
+
+class Login extends React.Component {
+  state = {
+    login: true,
+    username: '',
+    password: '',
+  };
+
+  componentDidMount() {
+    console.log('Login', this.state.login);
+  }
+
+  componentDidUpdate() {
+    console.log('Login', this.state.login);
+  }
+
+  render() {
+    const { login, username, password } = this.state;
+    return (
+      <div>
+        <h4>{login ? 'Login' : 'Signup'}</h4>
+        <div>
+          <input
+            value={username}
+            onChange={e => this.setState({ username: e.target.value })}
+            type="text"
+            placeholder="Username"
+          />
+
+          <input
+            value={password}
+            onChange={e => this.setState({ password: e.target.value })}
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <div>
+          <button onClick={() => this._confirm()}>
+            {login ? 'login' : 'create account'}
+          </button>
+          <button onClick={() => this.setState({ login: !login })}>
+            {login ? 'need to create an account?' : 'already have an account?'}
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  _confirm = async () => {
+    console.log('confirmed');
+    console.log('Login: ', this.state.login);
+  };
+
+  _saveUserData = username => {
+    localStorage.setItem(AUTH_TOKEN, username);
+  };
+}
 
 export default Login;
