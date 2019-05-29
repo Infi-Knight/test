407bbc134f853185ab5ec8511b183d807fdca041
Ravi Soni <soniravi829@gmail.com>
2019-05-30 00:12:58 +0530

    Use custom history for use outside Route

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
+import history from '../../history';
 import Header from '../Header';
 import Home from '../Home';
 import PrivateRoute from '../Auth/PrivateRoute';
 import Dashboard from '../Dashboard';
-import Login from '../Login';
+import LoginSignup from '../LoginSignup';
 import Error404 from '../Error404';
 
 const AppRouter = () => (
-  <Router>
+  <Router history={history}>
     <div>
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={Login} />
+        <Route path="/login" exact component={LoginSignup} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <Route component={Error404} />
       </Switch>
