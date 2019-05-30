f445726d095a6b275f9a9b3c7a18c6ee77dee07a
Ravi Soni <soniravi829@gmail.com>
2019-05-30 18:10:49 +0530

    Add Login logic

--- src/components/LoginSignup/Login.js
+++ src/components/LoginSignup/Login.js
@@ -1,56 +1,103 @@
-import React, { useState } from 'react';
-// import gql from 'graphql-tag';
+import React, { useState, useContext } from 'react';
+import { ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+import AuthContext from '../Auth';
 
-// const REVIEWER_LOGIN_QUERY = gql`
-//   query reviewerLogin($input: ReviewerWhereUniqueInput!) {
-//     reviewer(where: $input) {
-//       username
-//       id
-//       posts {
-//         title
-//         body
-//         image
-//       }
-//     }
-//   }
-// `;
+const REVIEWER_LOGIN_QUERY = gql`
+  query reviewerLoginQuery($input: ReviewerWhereUniqueInput!) {
+    reviewer(where: $input) {
+      username
+      password
+      id
+      posts {
+        title
+        body
+        image
+      }
+    }
+  }
+`;
 
-const Login = () => {
+const Login = props => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // const [error, setError] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const [error, setError] = useState('');
+  const { setAuthenticated } = useContext(AuthContext);
 
-  const _handleSubmit = e => {
+  const _handleSubmit = (e, client) => {
     e.preventDefault();
+    setLoggingIn(true);
+    // ApolloConsumer doesn't provide any error handling
+    client
+      .query({
+        query: REVIEWER_LOGIN_QUERY,
+        variables: { input: { username } },
+      })
+      .then(queryResult => _handleLogin(queryResult))
+      .catch(() => _handleLoginError());
+  };
+
+  const _handleLogin = queryResult => {
+    const { loading, data } = queryResult;
+    if (loading) {
+      setLoggingIn(true);
+    } else {
+      if (data) {
+        setLoggingIn(false);
+        const returnedPassword = data.reviewer.password;
+        if (returnedPassword === password) {
+          setAuthenticated(data.reviewer.id);
+          props.history.push('/dashboard');
+        } else {
+          setError('Invalid credentials');
+          setUsername('');
+          setPassword('');
+        }
+      }
+    }
+  };
+
+  const _handleLoginError = () => {
+    setLoggingIn(false);
+    setError('Error logging in: check credentials and your network connection');
+    setPassword('');
   };
 
   return (
     <div>
-      <h1>Login</h1>
-      {/* <h4>{error}</h4> */}
-      <form onSubmit={_handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            autoFocus
-            onChange={e => setUsername(e.target.value)}
-            id="username"
-            value={username}
-          />
-        </div>
+      <ApolloConsumer>
+        {client => (
+          <div>
+            <h1>Login</h1>
+            {loggingIn && <h4>Logging...</h4>}
+            <h4>{error}</h4>
+            <form onSubmit={e => _handleSubmit(e, client)}>
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  autoFocus
+                  onChange={e => setUsername(e.target.value)}
+                  id="username"
+                  value={username}
+                />
+              </div>
 
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            onChange={e => setPassword(e.target.value)}
-            id="password"
-            value={password}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  onChange={e => setPassword(e.target.value)}
+                  id="password"
+                  value={password}
+                />
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        )}
+      </ApolloConsumer>
     </div>
   );
 };
