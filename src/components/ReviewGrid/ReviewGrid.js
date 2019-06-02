4114c2d805c40d25dd60de45aee80ebe364a63fb
Ravi Soni <soniravi829@gmail.com>
2019-06-02 23:09:31 +0530

    Fix spacing and size

--- src/components/ReviewGrid/ReviewGrid.js
+++ src/components/ReviewGrid/ReviewGrid.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReviewItem from './ReviewItem';
-import './ReviewGrid.module.css';
+import ReviewGridStyles from './ReviewGrid.module.css';
 
 const ReviewGrid = props => {
   return (
-    <ul>
-      {props.posts.map(post => (
-        <ReviewItem
-          key={post.id}
-          title={post.title}
-          image={post.image}
-          body={post.body}
-        />
-      ))}
-    </ul>
+    <div className={ReviewGridStyles.GridContainer}>
+      <ul>
+        {props.posts.map(post => (
+          <li key={post.id}>
+            <ReviewItem
+              title={post.title}
+              image={post.image}
+              body={post.body}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/components/ReviewGrid/ReviewItem/ReviewItem.js
+++ src/components/ReviewGrid/ReviewItem/ReviewItem.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Card, StyledBody } from 'baseui/card';
+import { Card, StyledBody, StyledAction, StyledThumbnail } from 'baseui/card';
+import { Button } from 'baseui/button';
 
 const ReviewItem = props => {
   return (
     <Card
-      overrides={{ Root: { style: { width: '320px' } } }}
-      headerImage={props.image}
+      overrides={{
+        Root: {
+          style: { width: '320px' },
+        },
+      }}
       title={props.title}
     >
-      <StyledBody>{props.body}</StyledBody>
+      <StyledThumbnail src={props.image} />
+      <StyledBody>{`${props.body.substring(0, 64)}...`}</StyledBody>
+      <StyledAction>
+        <Button style={{ width: '100%' }}>More info</Button>
+      </StyledAction>
     </Card>
   );
 };
