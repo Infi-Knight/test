8080152d7737d46d0a479773268d7651f163a977
Ravi Soni <soniravi829@gmail.com>
2019-06-02 23:08:01 +0530

    Redesign header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,8 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import {
-  HeaderNavigation,
-  ALIGN,
-  StyledNavigationItem as NavigationItem,
-  StyledNavigationList as NavigationList,
-} from 'baseui/header-navigation';
-import { Button } from 'baseui/button';
 import history from '../../history';
 import AuthContext from '../Auth';
-import HeaderStyles from './Header.module.css';
+import styles from './Header.module.css';
 
 const Header = props => {
   const { authenticated, setAuthenticated } = useContext(AuthContext);
@@ -20,42 +13,29 @@ const Header = props => {
   };
 
   return (
-    <header>
-      <HeaderNavigation className={HeaderStyles.HeaderContainer}>
-        <NavigationList align={ALIGN.left}>
-          <NavigationItem>
-            <NavLink className={HeaderStyles.Brand} to="/">
-              TEST
+    <header className={styles.Header}>
+      <div className={styles.HeaderGroup}>
+        <div className={styles.HeaderRow}>
+          <NavLink className={styles.Brand} to="/">
+            TEST
+          </NavLink>
+          {!authenticated ? (
+            <NavLink to="/login">
+              <button>Login</button>
             </NavLink>
-          </NavigationItem>
-        </NavigationList>
-
-        <NavigationList align={ALIGN.right}>
-          <NavigationItem>
-            {!authenticated ? (
-              <NavLink to="/login">
-                <Button className={HeaderStyles.Button}>Login</Button>
-              </NavLink>
-            ) : (
-              <Button onClick={_handleLogout} className={HeaderStyles.Button}>
-                Logout
-              </Button>
-            )}
-          </NavigationItem>
-
-          <NavigationItem>
-            <NavLink to="/create">
-              <Button className={HeaderStyles.Button}>Write a review</Button>
-            </NavLink>
-          </NavigationItem>
-
-          <NavigationItem>
-            <NavLink to="/dashboard">
-              <Button className={HeaderStyles.Button}>Dashboard</Button>
-            </NavLink>
-          </NavigationItem>
-        </NavigationList>
-      </HeaderNavigation>
+          ) : (
+            <button onClick={_handleLogout}>Logout</button>
+          )}
+        </div>
+        <div className={styles.HeaderRow}>
+          <NavLink to="/create">
+            <button>Write a review</button>
+          </NavLink>
+          <NavLink to="/dashboard">
+            <button>Dashboard</button>
+          </NavLink>
+        </div>
+      </div>
     </header>
   );
 };
